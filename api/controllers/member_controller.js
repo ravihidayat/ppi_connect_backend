03a2f8cfe50159822ab2e74a66273a0db3e3d398
Barongobber <barongobirn@gmail.com>
2021-07-04 13:51:29 +0800

    Post method for backend
    change the get method for member auth into post method.

--- api/controllers/member_controller.js
+++ api/controllers/member_controller.js
@@ -4,14 +4,14 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 
 //For all or auth
-router.get('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
     try {
-        if(req.query.email != null && req.query.password != null){
-            const result = await memberModel.getByAuth(req.query.email)
+        if(req.body['email'] != null && req.body['password'] != null){
+            const result = await memberModel.getByAuth(req.body['email'])
             if(result == null) return res.json({})
             var hash = result['password']
             hash = hash.replace(/^\$2y(.+)$/i, '$2a$1')
-            bcrypt.compare(req.query.password, hash, function(error, compRes){
+            bcrypt.compare(req.body['password'], hash, function(error, compRes){
                 if(compRes) res.json(result)
                 else return res.json({})
             })
@@ -33,5 +33,10 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
+router.post('/coba', (req, res, next) => {
+    // var data = JSON.parse(req.body);
+    console.log(req.body['email'], req.body['password']); 
+    // console.log(req.body);
+})
 
 module.exports = router
